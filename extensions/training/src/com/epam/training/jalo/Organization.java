d168fc30e05a549c1b7d6c4243deec4f42664ffb
Anton Motruk <Anton_Motruk@epam.com>
2018-01-22 09:51:57 +0200

    add custom hmc

--- extensions/training/src/com/epam/training/jalo/Organization.java
+++ extensions/training/src/com/epam/training/jalo/Organization.java
@@ -6,10 +6,10 @@ import de.hybris.platform.jalo.SessionContext;
 import de.hybris.platform.jalo.type.ComposedType;
 import org.apache.log4j.Logger;
 
-public class Organisation extends GeneratedOrganisation
+public class Organization extends GeneratedOrganization
 {
 	@SuppressWarnings("unused")
-	private final static Logger LOG = Logger.getLogger( Organisation.class.getName() );
+	private final static Logger LOG = Logger.getLogger( Organization.class.getName() );
 	
 	@Override
 	protected Item createItem(final SessionContext ctx, final ComposedType type, final ItemAttributeMap allAttributes) throws JaloBusinessException
