eaa8cc666417ed0a5a33b75fb41a2bbc6c03cc81
Anton Motruk <anton.motruk@gmail.com>
2018-01-23 20:46:38 +0200

    Fix #8 and Fix #10

--- extensions/trainingstorefront/web/src/com/mycompany/training/storefront/controllers/pages/LoginPageController.java
+++ extensions/trainingstorefront/web/src/com/mycompany/training/storefront/controllers/pages/LoginPageController.java
@@ -14,8 +14,12 @@
 package com.mycompany.training.storefront.controllers.pages;
 
 import com.mycompany.training.storefront.service.CustomerStatusService;
+import de.hybris.platform.acceleratorstorefrontcommons.breadcrumb.Breadcrumb;
 import de.hybris.platform.acceleratorstorefrontcommons.controllers.pages.AbstractLoginPageController;
+import de.hybris.platform.acceleratorstorefrontcommons.controllers.pages.AbstractRegisterPageController;
 import de.hybris.platform.acceleratorstorefrontcommons.controllers.util.GlobalMessages;
+import de.hybris.platform.acceleratorstorefrontcommons.forms.GuestForm;
+import de.hybris.platform.acceleratorstorefrontcommons.forms.LoginForm;
 import de.hybris.platform.acceleratorstorefrontcommons.forms.RegisterForm;
 import de.hybris.platform.cms2.exceptions.CMSItemNotFoundException;
 import de.hybris.platform.cms2.model.pages.AbstractPageModel;
@@ -26,6 +30,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import de.hybris.platform.cms2.model.pages.ContentPageModel;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.context.annotation.Scope;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
@@ -38,16 +43,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.Collections;
+
 /**
  * Login Controller. Handles login and register for the account flow.
  */
 @Controller
 @Scope("tenant")
 @RequestMapping(value = "/login")
-public class LoginPageController extends AbstractLoginPageController
+public class LoginPageController extends AbstractRegisterPageController
 {
 	private HttpSessionRequestCache httpSessionRequestCache;
-	private static final String ACCOUNT_LOCKED_ERROR = "login.error.account.locked";
+	private static final String SPRING_SECURITY_LAST_USERNAME = "SPRING_SECURITY_LAST_USERNAME";
 	@Resource(name = "customerStatusService")
 	private CustomerStatusService customerStatusService;
 
@@ -89,12 +96,6 @@ public class LoginPageController extends AbstractLoginPageController
 		if (!loginError)
 		{
 			storeReferer(referer, request, response);
-		} else {
-			String username = (String) session.getAttribute("SPRING_SECURITY_LAST_USERNAME");
-			if (!customerStatusService.getCustomerStatus(username)) {
-				GlobalMessages.addErrorMessage(model, ACCOUNT_LOCKED_ERROR);
-				return getDefaultLoginPage(false, session, model);
-			}
 		}
 		return getDefaultLoginPage(loginError, session, model);
 	}
@@ -114,6 +115,43 @@ public class LoginPageController extends AbstractLoginPageController
 	{
 		getRegistrationValidator().validate(form, bindingResult);
 		return processRegisterUserRequest(referer, form, bindingResult, model, request, response, redirectModel);
+	}
+
+	private String getDefaultLoginPage(final boolean loginError, final HttpSession session, final Model model)
+			throws CMSItemNotFoundException
+	{
+		final LoginForm loginForm = new LoginForm();
+		model.addAttribute(loginForm);
+		model.addAttribute(new RegisterForm());
+		model.addAttribute(new GuestForm());
+
+		final String username = (String) session.getAttribute(SPRING_SECURITY_LAST_USERNAME);
+		if (username != null)
+		{
+			session.removeAttribute(SPRING_SECURITY_LAST_USERNAME);
+		}
+
+		loginForm.setJ_username(username);
+		storeCmsPageInModel(model, getCmsPage());
+		setUpMetaDataForContentPage(model, (ContentPageModel) getCmsPage());
+		model.addAttribute("metaRobots", "index,nofollow");
+
+		final Breadcrumb loginBreadcrumbEntry = new Breadcrumb("#", getMessageSource().getMessage("header.link.login", null,
+				"header.link.login", getI18nService().getCurrentLocale()), null);
+		model.addAttribute("breadcrumbs", Collections.singletonList(loginBreadcrumbEntry));
+
+		if (loginError)
+		{
+			model.addAttribute("loginError", Boolean.valueOf(loginError));
+
+			if (!customerStatusService.getCustomerStatus(username)) {
+				GlobalMessages.addErrorMessage(model, "login.error.account.locked");
+			} else {
+				GlobalMessages.addErrorMessage(model, "login.error.account.not.found.title");
+			}
+		}
+
+		return getView();
 	}
 
 	public CustomerStatusService getCustomerStatusService() {
